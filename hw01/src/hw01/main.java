9db8ddb75005ba23e6c3d058c48c25a9927499cc
Miffy <39194102+kkian481718@users.noreply.github.com>
2024-09-26 14:23:26 +0800

    Update main.java

--- hw01/src/hw01/main.java
+++ hw01/src/hw01/main.java
@@ -2,15 +2,14 @@ package hw01;
 
 public class main {
 	// 用字串轉數字
-    public static int Inverse_string(int x) {
+    public static int Invert2(int x) {
     	String z = String.valueOf(x);
     	String new_z = "";
     	for(int i = z.length(); i > 0; i--) {
     		new_z += z.substring(i-1, i);
     	}
     	
-    	int new_x = Integer.parseInt(new_z);
-    	return new_x;
+    	return Integer.parseInt(new_z);
     }
     
     public static void main(String[] args) {
